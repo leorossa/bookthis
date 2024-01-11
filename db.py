e88d4a697a41c8f1cd99079ab95f69d6d3abaf6b
Leonard <leo-testarossa@yandex.ru>
2024-01-11 22:28:59 +0300

    Update db.py
    Логика перенесена в основной файл

--- db.py
+++ db.py
@@ -20,22 +20,3 @@ def create_database():
 # Вызываем функцию для создания базы данных
 if __name__ == "__main__":
     create_database()
-
-#алгоритм поиска схожего названия если не хватит UNIQUE
-"""
-def insert_film(film, user_id):
-    cursor.execute('''
-                   SELECT COUNT(*) FROM films WHERE film = ? AND user_id = ?
-                   ''', (film, user_id))
-    count = cursor.fetchone()[0]
-    if count == 0:
-        cursor.execute('''
-                       INSERT INTO films (film, user_id) VALUES (?, ?)
-                       ''', (film, user_id))
-        conn.commit()
-        print("Фильм " + film + " добавлен")
-    else:
-        print("Фильм " + film + " уже был добавлен ранее")
-
-conn.close()
-"""
