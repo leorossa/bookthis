f6918cb41de227a26c35b64f6098d80a4d94f067
Leonard <leo-testarossa@yandex.ru>
2024-01-11 22:27:44 +0300

    Update telebot.py
    Показ меню после сообщения /start, отладка сохранения фильма, теперь он сравнивает есть ли фильм уже в базе и не сохраняет повтор. Добавление фильтров для исключения фраз и слов из поиска.

--- telebot.py
+++ telebot.py
@@ -16,9 +16,14 @@ ia = imdb.IMDb()
 
 #функция-обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Меню:" + "\n/start - запуск бота и меню" +\
-                                    "\nНапиши мне название фильма, я поищу такой его в базе" +\
-                                          "\n/getfilms - получить сохраненные фильмы")
+    keyboard = [
+            [KeyboardButton("Один случайный фильм")],
+            [KeyboardButton("Последние 5 добавленных")],
+        ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Для сохраниня фильма введите название",\
+                                    reply_markup=reply_markup)
 
 
 #функция- обработчик сохранения фильма
@@ -30,37 +35,33 @@ async def savefilm(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         if movie_title:
             movie = ia.get_movie(movie_title[0].movieID)
-            cursor.execute('INSERT INTO films (film, user_id) VALUES (?, ?)',\
-                            (movie['title'], update.effective_user.id))
-            conn.commit()
-            await context.bot.send_message(chat_id=update.effective_chat.id, text='Фильм ' + movie['title'] +\
-                                            ' ' + str(movie['year']) + ' года выпуска' + ' сохранен' +\
-                                            '\n/start - вернуться в меню')
+            cursor.execute('SELECT COUNT(*) FROM films WHERE film = ? AND user_id = ?',\
+                           (movie['title'], update.effective_user.id))
+            count = cursor.fetchone()[0]
+
+
+            if count == 0:
+                cursor.execute('INSERT INTO films (film, user_id) VALUES (?, ?)',
+                               (movie['title'], update.effective_user.id))
+                conn.commit()
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='Фильм ' +\
+                                                movie['title'] + ' ' + str(movie['year']) +' года выпуска сохранен')
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='Фильм ' +\
+                                                movie['title'] + ' уже сохранен')
         else:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="Фильм не найден" +\
-                                            "\n/start - вернуться в меню")
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Фильм не найден")
     except Exception as e:
         conn.rollback()
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная ошибка" +\
-                                            "\n/start - вернуться в меню")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная ошибка")
     finally:
         conn.close()
 
-
-#функция- обработчик команды /getfilms для получения списка сохраненных фильмов
-async def getfilms(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        keyboard = [
-            [KeyboardButton("Показать 1 случайный фильм")],
-            [KeyboardButton("Показать последние 5 добавленных фильмов")]
-        ]
-        reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
-    
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Выберите действие:", reply_markup=reply_markup)
-
+#функция выбора из меню
 async def handle_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
         choice = update.message.text
     
-        if choice == "Показать 1 случайный фильм":
+        if choice == "Один случайный фильм":
             conn = sqlite3.connect('films.db')
             cursor = conn.cursor()
             cursor.execute('SELECT film FROM films WHERE user_id = ?', (update.effective_user.id,))
@@ -73,7 +74,7 @@ async def handle_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 random_film = random.choice(films)
                 await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Случайный фильм: {random_film[0]}")
     
-        elif choice == "Показать последние 5 добавленных фильмов":
+        elif choice == "Последние 5 добавленных":
             conn = sqlite3.connect('films.db')
             cursor = conn.cursor()
             cursor.execute('SELECT film FROM films WHERE user_id = ? ORDER BY id DESC LIMIT 5', (update.effective_user.id,))
@@ -91,12 +92,14 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token('5376189132:AAGD207buqYz8yx8xH7EG9BSpQPrqtjW0aM').build()
     
     start_handler = CommandHandler('start', start)
-    getfilms_handler = CommandHandler('getfilms', getfilms)
-    savefilm_handler = MessageHandler(filters.TEXT, savefilm)
+    savefilm_handler = MessageHandler(~filters.Text("Один случайный фильм")\
+                                      & ~filters.Text("Последние 5 добавленных"), savefilm)
+    choice_handler = MessageHandler(filters.TEXT, handle_choice)
 
     application.add_handler(start_handler)
-    application.add_handler(getfilms_handler)
     application.add_handler(savefilm_handler)
+    application.add_handler(choice_handler)
+    
 
     
     application.run_polling()
